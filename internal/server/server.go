d3a360f829e59adb49a166921fdda7644f1546b6
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-06-30 07:57:29 +0900

    21: update cmd/httpserver/main.go and internal/server/server.go - modify codes to follow go convention

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,7 +13,7 @@ import (
 )
 
 type Server struct {
-	port     int
+	port     int //@@@ 예시의 경우 구조체에 port 저장 안함
 	handler  Handler
 	listener net.Listener
 	closed   atomic.Bool
@@ -28,6 +28,7 @@ type HandlerError struct {
 
 // Server 구조체를 초기화하고 반환하면서 Server.listen 메소드를 고 루틴으로 시작하는 함수
 func Serve(port int, handler Handler) (*Server, error) {
+	// @@@ 예시의 경우 어차피 *Server를 반환하므로 구조체 선언때도 &Server{}로 바로 포인터 생성
 	server := Server{
 		port:    port,
 		handler: handler,
@@ -39,6 +40,7 @@ func Serve(port int, handler Handler) (*Server, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error creating listener: %w", err)
 	}
+	// @@@ 예시의 경우 listener 생성을 먼저 하고 그 뒤에 Server 구조체 선언
 	server.listener = listener
 
 	// go 루틴으로 listen 루프 시작
@@ -68,17 +70,22 @@ func (s *Server) listen() {
 		if err != nil {
 			if s.closed.Load() {
 				// 서버가 닫히면서 s.listener.Accept()가 에러를 반환한 경우 for 루프 종료
-				break
+				// break
+				return
 			}
 			// 서버가 닫히지 않았는데 에러가 발생했다면 심각한 문제이므로 로그 후 종료
-			log.Fatalf("error accepting connection: %v", err)
+			// log.Fatalf("error accepting connection: %v", err)
+			// @@@ 예시처럼 에러 발생시에도 handle함수를 종료하지 않고 다음 for 루프로 넘어가기
+			log.Printf("error accepting connection: %v", err)
+			continue
 		}
 		// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 		// Accept 함수 에러처리 수정전에는 서버가 닫히면서 Accept함수가 에러를 발생하면
-		// err != nil && s.closed.Load() 상태가 예외 처리되지 않고 이 go s.handle(curConn) 까지 와서 에러가 발생했기때문에
+		// err != nil && s.closed.Load() 상태가 예외 처리되지 않고
+		// 이 go s.handle(curConn) 까지 와서 에러가 발생했기때문에
 		// if !s.closed.Load() 가 필요했었지만
-		// 이제 서버가 종료되면 위에서 break로 빠져나가므로 문제없음
+		// 이제 서버가 종료되면 위에서 함수를 빠져나가므로 문제없음
 		// if !s.closed.Load() {
 		// 	go s.handle(curConn)
 		// }
@@ -94,11 +101,22 @@ func (s *Server) handle(conn net.Conn) {
 	// internal/request의 RequestFromReader를 이용해 conn이 보낸 request 파싱
 	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		log.Fatalf("error parsing request: %v", err)
+		// log.Fatalf("error parsing request: %v", err)
+		// @@@ 예시를 따라 HandlerError 이용
+		WriteHandlerError(
+			&HandlerError{
+				StatusCode: response.StatusBadRequest,
+				Message:    []byte(err.Error()),
+			},
+			conn,
+		)
+		// @@@ log.Fatalf 대신 return
+		return
 	}
 
-	// handler가 response body를 저장할 버퍼 생성
+	// handler가 response body를 임시로 저장할 버퍼 생성
 	buffer := &bytes.Buffer{}
+	// @@@ 예시는 bytes.NewBuffer([]byte{}) 사용함
 	// io.Writer를 구현하는건 *bytes.Buffer
 
 	// handler 호출
@@ -111,20 +129,53 @@ func (s *Server) handle(conn net.Conn) {
 
 	err = response.WriteStatusLine(conn, response.StatusOK)
 	if err != nil {
-		log.Fatalf("error writing status line to connection: %v", err)
+		// log.Fatalf("error writing status line to connection: %v", err)
+		// @@@ 예시를 따라 HandlerError 이용
+		WriteHandlerError(
+			&HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message:    []byte(err.Error()),
+			},
+			conn,
+		)
+		// @@@ log.Fatalf 대신 return
+		return
 	}
 
 	headers := response.GetDefaultHeaders(buffer.Len())
 
 	err = response.WriteHeaders(conn, headers)
 	if err != nil {
-		log.Fatalf("error writing headers to connection: %v", err)
+		// log.Fatalf("error writing headers to connection: %v", err)
+		// @@@ 예시를 따라 HandlerError 이용
+		WriteHandlerError(
+			&HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message:    []byte(err.Error()),
+			},
+			conn,
+		)
+		// @@@ log.Fatalf 대신 return
+		return
 	}
 
 	// buffer(여기서는 io.Reader 구현체)에 저장된 response body의 내용을 conn(io.Writer)에 저장
 	_, err = io.Copy(conn, buffer)
+	// @@@ 예시는 b := buffer.Bytes()를 써서
+	// @@@ buffer의 아직 읽히지 않은 []bytes 타입 데이터 부분만 가져온 뒤
+	// @@@ io.Copy 대신 conn.Write(b)를 사용
 	if err != nil {
-		log.Fatalf("error writing response body to connection: %v", err)
+		// log.Fatalf("error writing response body to connection: %v", err)
+		// @@@ 예시를 따라 HandlerError 이용
+		WriteHandlerError(
+			&HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message:    []byte(err.Error()),
+			},
+			conn,
+		)
+		// @@@ log.Fatalf 대신 return
+		return
 	}
 
 	fmt.Printf("Successfully writes to the connection %v\n", conn.RemoteAddr())
@@ -144,6 +195,7 @@ func (s *Server) Close() error {
 }
 
 // HandlerError 구조체에 적힌 에러 정보를 io.Writer로 쓰는 함수
+// @@@ 예시의 경우 일반 함수대신 HandlerError의 메소드로 작성
 func WriteHandlerError(handlerError *HandlerError, w io.Writer) {
 	err := response.WriteStatusLine(w, handlerError.StatusCode)
 	if err != nil {
